5db700f2b4e58bb713cc81e58cd20df30175ec6a
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-10 21:10:32 -0500

    created a GET request for all burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,5 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+const burgers = require('../models/burgers.js');
+
+// Create all our routes and set up logic within those routes where required.
+router.get('/', (req, res) => {
+    burger.all((data) => {
+      const hbsObject = {
+        burgers: data,
+      };
+      console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
+
+
+  
+
+
+
+
+
+
+// Import the model (burgers.js) to use its database functions.
 module.exports = router;
