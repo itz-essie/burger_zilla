cbdc9d5f34ca16e6de99a60ac72bc2ec34ea52d1
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-10 21:14:40 -0500

    created UPDATE request

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -21,6 +21,25 @@ router.get('/', (req, res) => {
       res.json({ id: result.insertId });
     });
   });
+  router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log('condition', condition);
+  
+    burger.update(
+      {
+        devoured: req.body.devoured,
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
 
 
 
