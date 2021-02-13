bed60cb80cb7658f4347470b43c7f3ca55c9ba6c
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-13 18:36:52 -0500

    created burger block file

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,5 @@ VALUES (1, "Portabella Mushroom Burger", true), (2, "Bacon Cheeseburger", false)
 (3, "Turkey Burger", true), (4, "Chilli Burger", true), (5, "Patty Melt", true), 
 (6, "Salmon Burger", false), (7, "Bunless Burger", true), (8, "Hamburger", true), 
 (9, "Meat-less Burger", false);
+
+SELECT * FROM burgers;
